a5ee2d771f88db8142d8b1878e9b992830d8baef
prakashbisen0 <75722413+prakashbisen0@users.noreply.github.com>
2021-01-03 23:22:50 +0530

    my project project

--- PHP_MySQL/_settings.php
+++ PHP_MySQL/_settings.php
@@ -7,10 +7,10 @@
 
 	$AWSbuttonSN = 'A123BC456789DEFG';  // AWS IoT Button serial number
 
-	$dbHost = 'localhost';  // MySQL database host name or IP address
-	$dbName = 'iotDB';  // MySQL database name
-	$dbUsername = 'user';  // MySQL database user with read/write priveleges to the database defined above
-	$dbPassword = 'password';  // Password for MySQL database user defined above
+	$dbHost = 'employeereg.c5gfh7zgoi06.ap-south-1.rds.amazonaws.com';  // MySQL database host name or IP address
+	$dbName = 'employee';  // MySQL database name
+	$dbUsername = 'root';  // MySQL database user with read/write priveleges to the database defined above
+	$dbPassword = 'Password';  // Password for MySQL database user defined above
 
 	$dbPort = ini_get("mysqli.default_port"); 
 	$dbSocket = ini_get("mysqli.default_socket"); 
